e89916f0e0b52fbf4912d990fe05af8266e68fcc
root <root@DESKTOP-R8VK2HK.localdomain>
2021-01-24 13:56:13 -0600

    Starting UI

--- src/features/todos/todosSlice.js
+++ src/features/todos/todosSlice.js
@@ -1,14 +1,26 @@
+const initialState = [
+  { id: 0, text: 'Test text', completed: false },
+  { id: 1, text: 'Test text', completed: false }
+]
+
+function nextTodoId(todos) {
+  const maxId = todos.reduce((accumulator, todo) => Math.max(todo.id, accumulator), todos[0].id);
+  return maxId + 1;
+}
+
 export default function todosReducer(state = initialState, action) {
   switch (action.type) {
     case 'todos/todoAdded': {
-      return [
+      return {
         ...state,
+        data:[
         {
           id: nextTodoId(state),
           text: action.payload,
           completed: false
         }
       ]
+      }
     }
     case 'todos/todoToggled': {
       return state.map(todo => {
